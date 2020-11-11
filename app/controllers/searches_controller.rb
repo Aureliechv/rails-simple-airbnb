c2f422b1856109ccf6980c0aa3ba2227eababc5a
Aurelie Chauvin <aurelie.chauvin@essec.edu>
2020-11-12 00:41:33 +0100

    rails simple airbnb app done with css and working search bar

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,6 +1,6 @@
 class SearchesController < ApplicationController
   def index
     redirect_to flats_path if params[:query].empty?
-    @flats = Flat.where('name LIKE ?', params[:query])
+    @flats = Flat.where("name LIKE '%#{params[:query]}%'")
   end
 end
